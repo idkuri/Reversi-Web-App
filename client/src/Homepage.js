721573cf2ebbd308583cb11155cd6e0dae173049
idkuri <acaoy2@gmail.com>
2023-05-31 14:25:56 -0400

    deploy bugfix

--- client/src/Homepage.js
+++ client/src/Homepage.js
@@ -1,60 +1,55 @@
-import React, { useEffect, useState } from 'react';
-import anime from "animejs/lib/anime.es.js"
-import "./styles/homepage.css"
-
+import React, { useCallback, useEffect, useState } from 'react';
+import anime from "animejs/lib/anime.es.js";
+import "./styles/homepage.css";
 
 const Homepage = () => {
     const [columns, setColumns] = useState(0);
     const [rows, setRows] = useState(0);
     const [toggled, setToggled] = useState(true);
 
-    const createTile = index => {
-        return <div className="tile" key={index} onClick={() => {tileOnClick(index)}}/>;
-    };
-
-    const createGrid = quantity => {
-        return Array.from(Array(quantity)).map((_, index) => createTile(index));
-    };
-
-    const resizeHandler = () => {
-        const newColumns = Math.floor(document.body.clientWidth / 125);
-        const newRows = Math.floor(document.body.clientHeight / 125);
-        setColumns(newColumns);
-        setRows(newRows);
-    };
-
-    const tileOnClick = index => {
-        setToggled(!toggled);
-        console.log("Tile " + index + " clicked")
+    const tileOnClick = useCallback(index => {
+        setToggled(prevToggled => !prevToggled);
+        console.log("Tile " + index + " clicked");
         anime({
-            targets:".tile",
+            targets: ".tile",
             opacity: toggled ? 0 : 1,
             delay: anime.stagger(50, {
                 grid: [columns, rows],
                 from: index
             })
-        })
-    }
-    
+        });
+    }, [toggled, columns, rows]);
+
+    const createTile = useCallback(index => {
+        return <div className="tile" key={index} onClick={() => {tileOnClick(index)}}/>;
+    }, [tileOnClick]);
+
+    const createGrid = useCallback(quantity => {
+        return Array.from(Array(quantity)).map((_, index) => createTile(index));
+    }, [createTile]);
+
+    const resizeHandler = useCallback(() => {
+        const newColumns = Math.floor(document.body.clientWidth / 125);
+        const newRows = Math.floor(document.body.clientHeight / 125);
+        setColumns(newColumns);
+        setRows(newRows);
+    }, []);
+
     useEffect(() => {
         resizeHandler();
         window.addEventListener('resize', resizeHandler);
         return () => {
-          window.removeEventListener('resize', resizeHandler);
+            window.removeEventListener('resize', resizeHandler);
         };
-        
-        
-      }, [resizeHandler]);
-
+    }, [resizeHandler]);
 
     return (
         <div id="wrapper" className="wrapper">
-            {toggled? <p className='loading_font'>Click anywhere to get started!</p> : <></>}
-            <div className="grid" style={{'--columns':columns, '--rows': rows,}}>
+            {toggled ? <p className='loading_font'>Click anywhere to get started!</p> : <></>}
+            <div className="grid" style={{'--columns': columns, '--rows': rows}}>
                 {createGrid(columns * rows)}
             </div>
         </div>
-
     );
 };
 
