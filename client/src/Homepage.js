639fb4f9f4cdacd1091ccfd31814294de7ccc661
idkuri <acaoy2@gmail.com>
2023-06-01 12:42:52 -0400

    fixed http request url

--- client/src/Homepage.js
+++ client/src/Homepage.js
@@ -68,7 +68,7 @@ const Homepage = () => {
         event.preventDefault()
         const gameId = makeid(6)
         console.log("Your game id: " + gameId);
-        fetch("http://localhost:3000/sessions", {
+        fetch("http://150.136.34.45:3000/sessions", {
             method: "POST",
             headers: {
                 'Content-Type': 'application/json'
