78484a55f2d5bae3db8fe2244accdeed1fcc1ef9
Niome <niomedev@gmail.com>
2023-06-02 05:22:56 +0000

    [SSL] Added support for both HTTP and HTTPS requests

--- server/server.js
+++ server/server.js
@@ -4,10 +4,14 @@ const mongoose = require('mongoose');
 const sessions = require('./routes/sessions');
 const path = require('path');
 const cors = require('cors');
+const fs = require('fs');
+
+const http = require('http');
+const https = require('https');
+
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 3000;
 const uri = process.env.URI;
 
 //Enable CORS
@@ -16,6 +20,17 @@ app.use(cors());
 // Routing
 app.use( express.json() )
 app.use("/sessions", sessions)
+
+// Certificate
+const privateKey = fs.readFileSync(process.env.CERTPRIV, 'utf8');
+const certificate = fs.readFileSync(process.env.CERT, 'utf8');
+const ca = fs.readFileSync(process.env.CA, 'utf8');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
 
 // Connecting to the database
 mongoose.connect(uri, {
@@ -30,10 +45,19 @@ mongoose.connect(uri, {
   process.exit(1);
 })
 
+//
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
 // HTTP request listener
-app.listen(port, () => {
-  console.log(`Listening to this port: ${port}`)
+httpServer.listen(80, () => {
+  console.log(`Listening to this port: 80`)
 }) 
+
+httpsServer.listen(443, () => {
+	console.log('HTTPS Server running on port 443');
+});
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 
