691db44769d54e50738a9ce6dbaafa9a8df1b683
idkuri <acaoy2@gmail.com>
2023-06-05 01:44:18 -0400

    [Server] implemented turn based for first two players that joins

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const path = require('path');
 const cors = require('cors');
 const fs = require('fs');
 const sessionInfo = require("./models/sessionInfo");
+const { checkValidity, calculate } = require('./utils/moveCalculations.js');
 
 const http = require('http');
 const https = require('https');
@@ -76,23 +77,50 @@ httpsServer.listen(443, () => {
 	console.log('HTTPS Server running on port 443');
 });
 
+async function joinRoom(room, player) {
+  let me = null
+  let sessionState = await sessionInfo.find({ gameId: room});
+  if (sessionState.length == 0) {
+      res.status(404).json({error: "Game not found"})
+      return
+  }
+  else {
+      const game = sessionState[0];
+      if (game.player2.playerID === null) {
+        game.player2.playerID = player
+        me = 2
+        await game.save();
+      }
+      else if (game.player1.playerID === null) {
+        me = 1
+        game.player1.playerID = player
+        await game.save();
+      }
+      else {
+        me = 3
+      }
+
+  }
+  return me
+}
 
 io.on('connection', socket => {
-  console.log(`user: ${socket.id} connected`)
 
-  socket.on('joinRoom', (room) => {
-    console.log(`user: ${socket.id} joining room: ${room}`)
+  socket.on('joinRoom', async (room) => {
+    let me = await joinRoom(room, socket.id)
+    console.log(`user: ${socket.id} joining room: ${room} you are: ${me}`)
     socket.join(room)
 
     socket.on('move', async (room, player, row, column) => {
+      if(!(me === player)) return;
       const status = await move(room, player, row, column);
       if (status == 200)
         io.to(room).emit("updateSession", player, row, column)
     })
   })
 
-  socket.on('disconnect', () => {
-    console.log(`user: ${socket.id} disconnected`);
+  socket.on('disconnect', (reason) => {
+    console.log(`user: ${socket.id} disconnected for ${reason}`);
   });
 })
 
@@ -103,211 +131,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-
-// Calculates the new state of the game given the current state and the move
-function calculate(state, player, move) {
-
-  // Conditions need to be met: 
-  // 1) White vicinity
-  // 2) Same piece in range 
-  let array = state
-  array[move[0]][move[1]] = player
-  // Horizontal search
-  const hResult = searchHorizontal(array, player, move)
-  console.log(`Horizontal Start: ${hResult[0]} End: ${hResult[1]}`)
-  array = flipHorizontal(array, player, move[0], hResult[0], hResult[1]);
-
-
-  // Implement Vertical
-  const vResult = searchVertical(array, player, move)
-  console.log(`Vertical Start: ${vResult[0]} End: ${vResult[1]}`)
-  array = flipVertical(array, player, move[1], vResult[0], vResult[1]);
-
-  // Implement Diagonal
-  const dResult = searchDiagonal(array, player, move)
-  array = flipDiagonal(array, player, dResult[0], dResult[1]);
-
-  return array;
-}
-
-function searchHorizontal(state, player, move) {
-  let start = move[1]
-  let end = move[1]
-
-  for (let i = move[1]-1; i >= 0 ; i--) {
-    if (state[move[0]][i] == 0) {
-      break
-    }
-    if (state[move[0]][move[1]-1] == player) {
-      break
-    }
-    if (state[move[0]][i] == player) {
-      start = i
-      break
-    }
-  }
-
-
-  for (let i = move[1]+1; i < 8; i++) {
-    if (state[move[0]][i] == 0) {
-      break
-    }
-    if (state[move[0]][move[1]+1] == player) {
-      break
-    }
-    if (state[move[0]][i] == player) {
-      end = i
-      break
-    }
-  }
-  return [start, end]
-}
-
-function searchVertical(state, player, move) {
-  let start = move[0]
-  let end = move[0]
-  for (let i = move[0]-1; i >= 0 ; i--) {
-    if (state[i][move[1]] == 0) {
-      break
-    }
-    if (state[move[0]-1][move[1]] == player) {
-      break
-    }
-    if (state[i][move[1]] == player) {
-      start = i
-      break
-    }
-  }
-  for (let i = move[0]+1; i < 8; i++) {
-    if (state[i][move[1]] == 0) {
-      break
-    }
-    if (state[move[0]+1][move[1]] == player) {
-      break
-    }
-    if (state[i][move[1]] == player) {
-      end = i
-      break
-    }
-  }
-  return [start, end]
-}
-
-function searchDiagonal(state, player, move) {
-  // DR short hand for down right
-  // TR short hand for top right
-  let DRstart = move
-  let DRend = move
-  let TRstart = move
-  let TRend = move
-
-  // [negative \] < relative to the columns
-  for (let i = move[0]-1, j = move[1]-1; i >= 0 && j >= 0  ; i--, j--) {
-    if (state[i][j] == 0) {
-      break
-    }
-    if (state[move[0]-1][move[1]-1] == player) {
-      break
-    }
-    if (state[i][j] == player) {
-      DRstart = [i, j]
-      break
-    }
-  }
-  // [positive \] < relative to the columns
-  for (let i = move[0]+1, j = move[1]+1; i < 8 && j < 8; i++, j++) {
-    if (state[i][j] == 0) {
-      break
-    }
-    if (state[move[0]+1][move[1]+1] == player) {
-      break
-    }
-    if (state[i][j] == player) {
-      DRend = [i, j]
-      break
-    }
-  }
-  // // [positive /] < relative to the columns
-  for (let i = move[0]-1, j = move[1]+1; i >= 0 && j < 8  ; i--, j++) {
-    if (state[i][j] == 0) {
-      break
-    }
-    if (state[move[0]-1][move[1]+1] == player) {
-      break
-    }
-    if (state[i][j] == player) {
-      TRend = [i, j]
-      break
-    }
-  }
-  // // [negative /] < relative to the columns
-  for (let i = move[0]+1, j = move[1]-1; i < 8 && j >= 0  ; i++, j--) {
-    if (state[i][j] == 0) {
-      break
-    }
-    if (state[move[0]+1][move[1]-1] == player) {
-      break
-    }
-    if (state[i][j] == player) {
-      TRstart = [i, j]
-      break
-    }
-  }
-  return [[DRstart, DRend], [TRstart, TRend]]
-}
-
-function flipHorizontal(state, player, row, start, end) {
-  let array = state
-  for (let i = start; i <= end; i++ ) {
-    array[row][i] = player
-  }
-  return array;
-}
-
-function flipVertical(state, player, column, start, end) {
-  let array = state
-  for (let i = start; i <= end; i++ ) {
-    array[i][column] = player
-  }
-  return array;
-}
-
-function flipDiagonal(state, player, DR, TR) {
-  const DRstart = DR[0]
-  const DRend = DR[1]
-  const TRstart = TR[0]
-  const TRend = TR[1]
-
-  let array = state
-  for (let i = DRstart[0], j = DRstart[1]; i <= DRend[0] && j <= DRend[1]; i++, j++) {
-    array[i][j] = player
-  }
-  for (let i = TRstart[0], j = TRstart[1]; i >= TRend[0] && j <= TRend[1]; i--, j++) {
-    array[i][j] = player
-  }
-  return array;
-}
-
-function checkValidity(state, player, move, array) {
-  let result = array
-    const hResult = searchHorizontal(state, player, move)
-    const vResult = searchVertical(state, player, move)
-    const dResult = searchDiagonal(state, player, move)
-    const cond1 = (hResult[0] - hResult[1]) === 0 // true means no flip
-    const cond2 = (vResult[0] - vResult[1]) === 0 // true means no flip
-    const DRstart = dResult[0][0]
-    const DRend = dResult[0][1]
-    const TRstart = dResult[1][0]
-    const TRend = dResult[1][1];
-    const DReq = (DRstart[0] === DRend[0]) && (DRstart[1] === DRend[1])
-    const TReq = (TRstart[0] === TRend[0]) && (TRstart[1] === TRend[1])
-    const cond3 = DReq && TReq // true means no flip
-    console.log(cond1 && cond2 && cond3 ? "invalid" : "valid");
-    if (cond1 && cond2 && cond3) {{
-      return false
-    }}
-  return true // return array of position that has
-}
 
 async function move(room, current, row, column) {
     try {
